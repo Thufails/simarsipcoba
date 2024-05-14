3a52447ed0f3881b16b0a7836838a221d5f29ac8
Thufails <abahc4mp3x@gmail.com>
2024-05-14 10:11:37 +0700

    change request input

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -344,79 +344,26 @@ class PermissionController extends Controller
     }
 
 
-    public function requestInput(Request $request, $ID_ARSIP)
+    public function requestInput(Request $request)
     {
         $userRequestingId = Auth::user(); // ID pengguna yang meminta akses
-
-            $document = Arsip::with('jenisDokumen')
-                      ->with([
-                          'infoArsipPengangkatan',
-                          'infoArsipSuratPindah',
-                          'infoArsipPerceraian',
-                          'infoArsipPengesahan',
-                          'infoArsipKematian',
-                          'infoArsipKelahiran',
-                          'infoArsipPengakuan',
-                          'infoArsipPerkawinan',
-                          'infoArsipKk',
-                          'infoArsipSkot',
-                          'infoArsipSktt',
-                          'infoArsipKtp'
-                      ])->find($ID_ARSIP);
-
-        // Validasi apakah dokumen ditemukan
-        if (!$document) {
-            return response()->json(['message' => 'Dokumen tidak ditemukan'], 404);
-        }
-
-        $nama= [];
-        // Mendefinisikan array asosiasi model dengan nama kolom yang sesuai
-        $models = [
-            'infoArsipPengangkatan' => ['NAMA_ANAK'],
-            'infoArsipSuratPindah' => ['NAMA_KEPALA'],
-            'infoArsipPerceraian' => ['NAMA_PRIA'],
-            'infoArsipPengesahan' => ['NAMA_ANAK'],
-            'infoArsipKematian' => ['NAMA'],
-            'infoArsipKelahiran' => ['NAMA'],
-            'infoArsipPengakuan' => ['NAMA_ANAK'],
-            'infoArsipPerkawinan' => ['NAMA_PRIA', 'NAMA_WANITA'],
-            'infoArsipKk' => ['NAMA_KEPALA'],
-            'infoArsipSkot' => ['NAMA'],
-            'infoArsipSktt' => ['NAMA'],
-            'infoArsipKtp' => ['NAMA'],
-        ];
-
-        // Periksa ID_ARSIP dan tentukan model yang sesuai
-        foreach ($models as $relation => $columns) {
-            // Cek apakah relasi tersedia dan setidaknya satu dokumen tidak kosong
-            if ($document->$relation) {
-                foreach ($columns as $column) {
-                    if (!empty($document->$relation->$column)) {
-                        // Tambahkan dokumen ke dalam array
-                        $nama[] = $document->$relation->$column;
-                    }
-                }
-            }
-        }
 
         // Proses permintaan Scan
         $permissionRequest = new Permission();
         $permissionRequest->ID_OPERATOR = $userRequestingId;
-        $permissionRequest->ID_ARSIP = $document->ID_ARSIP;
         $permissionRequest->STATUS = 'Request Input';
         $permissionRequest->save();
 
         if ($permissionRequest) {
             return response()->json([
                 'success' => true,
-                'message' => 'Permintaan Scan berhasil diajukan. Menunggu Arsiparis.',
-                'nama_pemilik' => $nama,
+                'message' => 'Permintaan Input berhasil Diajukan. Menunggu Arsiparis.',
                 'data' => $permissionRequest,
             ], 201);
         } else {
             return response()->json([
                 'success' => false,
-                'message' => 'Permintaan Scan gagal diajukan.',
+                'message' => 'Permintaan Input gagal diajukan.',
                 'data' => ''
             ], 400);
         }
