316c8670e46fc9ff5ef1e6ea7ac6705112596807
Thufails <abahc4mp3x@gmail.com>
2024-05-17 10:48:47 +0700

    delete some routes

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,6 @@ $router->group(['prefix' => 'auth'], function () use ($router) {
 
 $router->group(['prefix' => 'dashboard'], function () use ($router) {
     $router->get('/', 'DashboardController@index');
-    $router->get('rekapitulasi', 'DashboardController@rekapitulasi');
-    $router->get('requestToday', 'DashboardController@requestToday');
-    $router->get('requestTotal', 'DashboardController@requestTotal');
-    $router->post('onlineUser', 'DashboardController@onlineUser');
     $router->post('logout', 'DashboardController@login');
 
 });
