4bd7787264057d8d2d5489050d2ac4a090d1456b
Thufails <abahc4mp3x@gmail.com>
2024-05-22 10:32:24 +0700

    coba authenticable

--- app/Models/Operator.php
+++ app/Models/Operator.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
 
-class Operator extends Model
+class Operator extends Model implements AuthenticatableContract, AuthorizableContract
 {
+    use Authenticatable, Authorizable, HasFactory;
     protected $table = 'operator';
     protected $primaryKey = 'ID_OPERATOR';
     public $timestamps = true;
