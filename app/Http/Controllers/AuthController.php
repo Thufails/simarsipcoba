150cc2f0dfabf004bd000f4338530d5492283376
Thufails <abahc4mp3x@gmail.com>
2024-05-28 09:59:51 +0700

    change 6 jam

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -81,7 +81,7 @@ class AuthController extends Controller
         Session::where('ID_OPERATOR', $operator->ID_OPERATOR)->delete();
 
         // Set waktu kedaluwarsa token (misalnya, 6 jam)
-        $expirationTimeInSeconds = 60 * 60; // 5 menit dalam detik
+        $expirationTimeInSeconds = 6 * 60 * 60; // 6 jam dalam detik
         // Hitung waktu kedaluwarsa dalam detik sejak epoch
         $expirationTime = time() + $expirationTimeInSeconds;
         // Konversi durasi waktu kedaluwarsa menjadi format jam
